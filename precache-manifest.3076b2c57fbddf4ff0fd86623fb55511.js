0f51345be3e71fb295314468bc7b2c3cb175a2ac
adrianbparra <adrianplane12@gmail.com>
2020-10-22 19:42:04 -0700

    Updates

---
+++ precache-manifest.3076b2c57fbddf4ff0fd86623fb55511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56c903ee3b5962e8eccc51c0c2153f4",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "b7b8457f931d29bd1c46",
+    "url": "/game-of-life/static/css/main.e56d40a9.chunk.css"
+  },
+  {
+    "revision": "27b4b96fa05d30bd1b89",
+    "url": "/game-of-life/static/js/2.5ccedfeb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/game-of-life/static/js/2.5ccedfeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b8457f931d29bd1c46",
+    "url": "/game-of-life/static/js/main.3ba2c653.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);

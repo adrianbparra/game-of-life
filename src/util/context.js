ace6f6590b28f0502c84337853b9bb8483e453b7
adrianbparra <adrianplane12@gmail.com>
2021-03-02 19:06:36 -0700

    Update context.js
    Creating of context with reducer. Will be updating if dispatch and state should be in different context.

--- src/util/context.js
+++ src/util/context.js
@@ -1,10 +1,32 @@
 import React, {useReducer, createContext} from "react"
+import { gridCreation } from "./gridCreation.js";
+import { reducer, initialState } from "./reducer.js";
 
 
-const initialState = {
+const GridContext = createContext({
     "generation": 0,
     "size": 15,
-    "grid": []
+    "grid": gridCreation(15)
+})
+
+
+
+function GridProvider(props) {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+
+    function clearGrid() {
+        console.log("clear grid provider")
+        dispatch({type: "CLEAR_GRID"})
+    }
+
+
+    return (
+        <GridContext.Provider
+            value = {{state, clearGrid}}
+            {...props}
+        />
+    )
 }
 
-
+export { GridProvider }

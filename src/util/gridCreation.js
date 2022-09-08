bf047ba3b3544e6081a652b6a1962fd999bd00d2
adrianbparra <adrianplane12@gmail.com>
2022-09-07 19:56:05 -0700

    Update gridCreation.js
    Removed comments

--- src/util/gridCreation.js
+++ src/util/gridCreation.js
@@ -32,19 +32,11 @@ function nextGeneration(grid) {
         const row = []
         for(let j = 0; j < copyGrid[i].length; j++){
             
-            // const col = {'alive': false, 'x' : j,'y' : i}
-            // check the values here and update the cell
-            // we only update this cell based on other cells around it
-
             
             const col = {'alive': false, 'x' : j,'y' : i}
-            
-            
-            // if there is more than 4 neighbours continue to next j since cell will
-            // dead
-            // adding up live neighbours
+              
             var liveNeighbours = 0
-            // top
+
             if (i - 1 >= 0 && j-1 >= 0 && copyGrid[i-1][j-1].alive){
 
                 liveNeighbours += 1
@@ -79,16 +71,13 @@ function nextGeneration(grid) {
             // current cell alive
             if (copyGrid[i][j].alive) {
                 
-                // two or three live neighbours lives on to the next generation.
-                // fewer than two live neighbours dies, as if by underpopulation.
-                // more than three live neighbours dies, as if by overpopulation.
                 if (liveNeighbours === 2 || liveNeighbours === 3) {
-                    // cell keeps living
+
                     col.alive = true
                 }
 
             } else {
-                // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
                 if (liveNeighbours === 3){
                     col.alive = true
                 }
